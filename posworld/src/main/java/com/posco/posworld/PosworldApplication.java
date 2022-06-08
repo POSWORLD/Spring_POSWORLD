e613478cea4aec5b7ea32d269cebe15f59fc4bb7
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-06-08 23:48:09 +0900

    first init

---
+++ posworld/src/main/java/com/posco/posworld/PosworldApplication.java
@@ -0,0 +1,13 @@
+package com.posco.posworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosworldApplication.class, args);
+	}
+
+}
---
+++ posworld/src/test/java/com/posco/posworld/PosworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posco.posworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
