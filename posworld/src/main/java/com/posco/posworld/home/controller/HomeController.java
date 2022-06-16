11223ef373278d41b54bea988cd42e24d2758a77
hyunmin <qweasx10@hanmail.net>
2022-06-16 11:57:22 +0900

    all complete

--- posworld/src/main/java/com/posco/posworld/home/controller/HomeController.java
+++ posworld/src/main/java/com/posco/posworld/home/controller/HomeController.java
@@ -3,9 +3,12 @@ package com.posco.posworld.home.controller;
 import com.posco.posworld.aspect.TokenRequired;
 import com.posco.posworld.config.SecurityService;
 import com.posco.posworld.home.model.HomeDto;
+
 import com.posco.posworld.home.service.HomeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("home")
@@ -17,15 +20,21 @@ public class HomeController {
     SecurityService securityService;
     @Autowired
     HomeDto homeDto;
+    @GetMapping("/{id}")
+    public HomeDto getHome(@PathVariable String id){
+        homeDto.setUserId(Integer.valueOf(id));
+        return homeService.getHome(homeDto);
+    }
+
     @PostMapping("/")
-    public Integer InserPost(@RequestBody HomeDto homeDto){
-        homeDto.setUserId(securityService.getIdAtToken());
+    public Integer InsertHome(@RequestBody HomeDto homeDto){
+
         return homeService.InsertHome(homeDto);
     }
-    @PutMapping("/{id}")
-    public Integer updatePost(@RequestBody HomeDto homeDto, @PathVariable String id){
+    @PutMapping("/up")
+    public Integer updateHome(@RequestBody HomeDto homeDto){
         homeDto.setUserId(securityService.getIdAtToken());
-        homeDto.setId(Integer.valueOf(id));
+        System.out.println(homeDto);
         return homeService.UpdateHome(homeDto);
     }
 }
