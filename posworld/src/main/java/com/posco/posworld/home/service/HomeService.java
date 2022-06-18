52e92dceb03f3d63e694a1c0c39f1d37cb3e3da6
qweasx10 <qweasx10@hanmail.net>
2022-06-18 11:25:20 +0900

    jpa finish

--- posworld/src/main/java/com/posco/posworld/home/service/HomeService.java
+++ posworld/src/main/java/com/posco/posworld/home/service/HomeService.java
@@ -1,15 +1,18 @@
 package com.posco.posworld.home.service;
 
 import com.posco.posworld.home.model.HomeDto;
+import com.posco.posworld.user.model.UserDto;
 
 
 import java.util.List;
 
 public interface HomeService {
 
-//    Integer insertHome(HomeDto homeDto);
-//    Integer updateHome(HomeDto homeDto);
+
     HomeDto getHome(Integer id);
     HomeDto insertHome(HomeDto homeDto);
+    HomeDto updateHome(HomeDto homeDto);
+
+
 
 }
--- posworld/src/main/java/com/posco/posworld/user/repository/UserRepository.java
+++ posworld/src/main/java/com/posco/posworld/user/repository/UserRepository.java
@@ -5,7 +5,7 @@ import com.posco.posworld.user.model.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<UserDto, Integer> {
