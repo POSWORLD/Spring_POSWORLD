abe8b587d17bfa07d2cff245643a73a3567e14f6
HjuH <hjuhee9807@naver.com>
2022-06-19 14:00:01 +0900

    photo change return value

--- posworld/src/main/java/com/posco/posworld/photo/repository/PhotoRepository.java
+++ posworld/src/main/java/com/posco/posworld/photo/repository/PhotoRepository.java
@@ -3,9 +3,11 @@ package com.posco.posworld.photo.repository;
 import com.posco.posworld.photo.model.PhotoDto;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -14,4 +16,10 @@ public interface PhotoRepository extends JpaRepository<PhotoDto, Integer> {
 
 
     List<PhotoDto> findPhotoDtoByUserid(@Param("userid")int userid);
+
+    @Modifying
+    @Transactional
+    @Query("Delete from PhotoDto p where p.id = ?1 and p.userid = ?2")
+    Integer deleteByIds(@Param("id") Integer id, @Param("userid") Integer userid);
+
 }
